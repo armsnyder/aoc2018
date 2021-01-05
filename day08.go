059f6fa765440a5ef933f3ae265e13f3078068ac
Adam Snyder <asnyder@ea.com>
2021-01-04 18:25:05 -0800

    Day 8 part 1 with stack methods
    Memory usage is identical to previous implementation

--- day08.go
+++ day08.go
@@ -15,32 +15,38 @@ var _ = declareDay(8, func(part2 bool, inputReader io.Reader) interface{} {
 })
 
 func day08Part1(inputReader io.Reader) interface{} {
-	var stack []day08Node
+	var stack day08Stack
 	var state day08State
 	total := 0
 
 	day08ScanNumbers(inputReader, func(v int) {
 		switch state {
 		case day08ChildHeader:
-			stack = append(stack, day08Node{children: v})
+			stack.push(day08Node{children: v})
 			state = day08MetadataHeader
+
 		case day08MetadataHeader:
-			stack[len(stack)-1].metadata = v
-			if stack[len(stack)-1].children == 0 {
+			node := stack.pop()
+			node.metadata = v
+			if node.children == 0 {
 				state = day08Metadata
 			} else {
-				stack[len(stack)-1].children--
+				node.children--
 				state = day08ChildHeader
 			}
+			stack.push(node)
+
 		case day08Metadata:
 			total += v
-			stack[len(stack)-1].metadata--
-			if stack[len(stack)-1].metadata == 0 {
-				stack = stack[:len(stack)-1]
-				if len(stack) > 0 && stack[len(stack)-1].children > 0 {
-					stack[len(stack)-1].children--
-					state = day08ChildHeader
-				}
+			node := stack.pop()
+			node.metadata--
+			if node.metadata > 0 {
+				stack.push(node)
+			} else if stack.peek().children > 0 {
+				node = stack.pop()
+				node.children--
+				stack.push(node)
+				state = day08ChildHeader
 			}
 		}
 	})
@@ -88,4 +94,26 @@ const (
 type day08Node struct {
 	children int
 	metadata int
+}
+
+type day08Stack []day08Node
+
+func (s *day08Stack) push(node day08Node) {
+	*s = append(*s, node)
+}
+
+func (s *day08Stack) pop() day08Node {
+	if len(*s) == 0 {
+		return day08Node{}
+	}
+	node := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return node
+}
+
+func (s *day08Stack) peek() day08Node {
+	if len(*s) == 0 {
+		return day08Node{}
+	}
+	return (*s)[len(*s)-1]
 }
